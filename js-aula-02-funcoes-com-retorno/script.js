7380e626187b0c6d27b0f4efbfabc6c0acb900d4
Laura Julia Mathias Amaral <laurajulia408@gmail.com>
2026-08-06 19:13:18 -0300

    Adicionando uma função com mais de um retorno, com um algoritmo que define se um número é ímpar ou par.

--- js-aula-02-funcoes-com-retorno/script.js
+++ js-aula-02-funcoes-com-retorno/script.js
@@ -1,12 +1,26 @@
-// Funções com retorno: 
-const n1 = 50; 
-const n2 = 3; 
+// Funções com retorno:
+const n1 = 50;
+const n2 = 3;
 
-// ESSA FUNÇÃO GERA UM RESULTADO E EXPORTA 
+// ESSA FUNÇÃO GERA UM RESULTADO E EXPORTA
 
 function miltiplicar(n1, n2) {
-    return n1 * n2; // * - Operador de muliplicação; 
+  return n1 * n2; // * - Operador de muliplicação;
 }
 
-// EXIBINDO O RESULTADO: 
-document.write(`<p>A multipliação de ${n1} por ${n2}é igual a <strong>${multiplicar(n1, n2)} </strong></p>`)
+// EXIBINDO O RESULTADO:
+document.write(
+  `<p>A multipliação de ${n1} por ${n2}é igual a <strong>${multiplicar(n1, n2)} </strong></p>`,
+);
+
+//FUNCOES COM MAIS DE UM RETORNO:
+
+const number = 4;
+
+function parImpar(number) {
+  if (number % 2 == 0) {
+    return "par";
+  } else {
+    return "impar";
+  }
+}
