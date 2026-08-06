44eba836530a455da258d2c97cff8a829cd287c3
Laura Julia Mathias Amaral <laurajulia408@gmail.com>
2026-08-06 19:05:53 -0300

    starting the javascript code for "Multiplicar"

---
+++ js-aula-02-funcoes-com-retorno/script.js
@@ -0,0 +1,12 @@
+// Funções com retorno: 
+const n1 = 50; 
+const n2 = 3; 
+
+// ESSA FUNÇÃO GERA UM RESULTADO E EXPORTA 
+
+function miltiplicar(n1, n2) {
+    return n1 * n2; // * - Operador de muliplicação; 
+}
+
+// EXIBINDO O RESULTADO: 
+document.write(`<p>A multipliação de ${n1} por ${n2}é igual a <strong>${multiplicar(n1, n2)} </strong></p>`)
