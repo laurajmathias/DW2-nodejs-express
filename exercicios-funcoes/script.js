213b34168c6b271c3c7b3a9350e9151ca45109d9
Laura Julia Mathias Amaral <laurajulia408@gmail.com>
2026-08-12 20:40:08 -0300

    exercicio 2 comitado

--- exercicios-funcoes/script.js
+++ exercicios-funcoes/script.js
@@ -1,12 +1,31 @@
 //EXERCICIO 1) Crie uma função simples que exiba no console seu nome, sua idade e sua cidade. (Não esqueça de chamar a função no final) 
 
-document.write(`<p> Exercicio 1)</p>`);
+document.write(`<h3> Exercicio 1)</h3>`);
 
 
-function showMessage() {
-    const nome = (Laura);   
+function mostrarDados() {
+   document.write(`nome: Laura`); 
+   document.write(`idade: 18 anos`); 
+   document.write(`cidade: Sete Barras`); 
+   
+   mostrarDados(); 
 
 }
+
+//EXERCICO 2)  Crie uma função que receba dois números como parâmetro em seguida faça a divisão entre eles. O resultado deve ser exibido no console concatenado com uma string. (Ex: “O resultado da divisão foi...”). A função deve ser chamada ao final passando dois números como argumento.
+
+
+document.write(`<h3>Exercicio 2</h3>`); 
+
+const n1 = 10; 
+const n2 = 5; 
+
+function divisão(n1, n2) {
+    let resultado = n1 / n2; 
+    document.write(`A divisão de ${n1} / ${n2} é igual a: ${resultado}.`); 
+}
+
+divisão(n1, n2); 
 
 
 
