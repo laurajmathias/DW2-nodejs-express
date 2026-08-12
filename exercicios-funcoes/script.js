88e854db588f3cf93165421c6f8d06283a01207c
Laura Julia Mathias Amaral <laurajulia408@gmail.com>
2026-08-12 20:08:42 -0300

    resolução exercicio 1) Javascript

---
+++ exercicios-funcoes/script.js
@@ -0,0 +1,11 @@
+//EXERCICIO 1) Crie uma função simples que exiba no console seu nome, sua idade e sua cidade. (Não esqueça de chamar a função no final) 
+
+document.write(`<p> Exercicio 1)</p>`);
+
+
+function showMessage() {
+  const nome = Laura;
+  document.write(nome);
+}
+
+
