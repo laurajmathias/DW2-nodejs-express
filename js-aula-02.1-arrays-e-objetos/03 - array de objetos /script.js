1de3ce562ecc635c62a634f7667919bc074fad6a
Laura Julia Mathias Amaral <laurajulia408@gmail.com>
2026-08-19 20:36:09 -0300

    criando uma array objetos

--- js-aula-02.1-arrays-e-objetos/03 - array de objetos /script.js
+++ js-aula-02.1-arrays-e-objetos/03 - array de objetos /script.js
@@ -10,3 +10,28 @@ document.write('<p>${produto}</p>');
 
 //ACESSANDO OS ATRIBUTOS DO OBJETO:
 document.write('<p> $%{produto.nome}, ${produto.marca}, custa R$ ${produto.preco}, <br> ${^produto.descricao} </p>'); 
+
+// ARRAY DE OBJETOS:
+
+document.write("<h3>Array de Objetos> </h3>"); 
+const listaProdutos = [
+    {
+    nome: "Computador", 
+    marca: "Lenovo", 
+    preco: 3000, 
+    descricao: "PC moderno e com bom desempenho",
+    }, 
+    {
+    nome: "Tablet", 
+    marca: "Samsumg", 
+    preco: 2000, 
+    descricao: "òtima velocidade de processamento", 
+    }, 
+    {
+    nome: "Iphone 17", 
+    marca: "Apple", 
+    preco: 17000, 
+    descricao: "Não quebra nunca", 
+    }]; 
+
+    
