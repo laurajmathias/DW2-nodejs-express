f125a0463760b09d99ffbeb8057696ba0043efd3
Laura Julia Mathias Amaral <laurajulia408@gmail.com>
2026-08-19 19:21:55 -0300

    testando

---
+++ js-aula-02.1-arrays-e-objetos/script.js
@@ -0,0 +1,17 @@
+// VETORES NO JAVASCRIPT 
+
+let produtos = [ "Computador", "Notebook", "Celular", "Tablet"] 
+
+document.write('O tipo da variável que recebe um vetot no Javascript é: <strong> ${typeof(produtos)} </strong>'); 
+
+document.write('<br> <br> O tipo da variável aluno é: <strong> ${typeof(aluno)} </strong>'); 
+
+
+//  ${typeof(produtos)}
+
+document.write("Exibindo os itens do vetor pelo índice : <br>"); 
+
+document.write('<p>${produtos[0]}</p>')
+document.write('<p>${produtos[1]}</p>')
+document.write('<p>${produtos[2]}</p>')
+document.write('<p>${produtos[3]}</p>')
