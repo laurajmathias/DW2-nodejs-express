c721a6be09f09edbb869ddeb05aaf0d47dd61c75
Laura Julia Mathias Amaral <laurajulia408@gmail.com>
2026-08-19 19:48:49 -0300

    continuandoi a aula

---
+++ js-aula-02.1-arrays-e-objetos/02 - manipulando vetores/script.js
@@ -0,0 +1,14 @@
+// MÉTODOS DE MANIPULAÇÃO DE VETORES: 
+
+let frutas = ['Laranja', 'Maçã', 'Banana']
+
+document.write('<p> Os itens do nosso vetor são: ${frutas} </p>'); 
+
+frutas[3] = 'Morango'; 
+
+document.write('<p> Agora a lista é: ${frutas'); 
+
+// MÉTODO PARA MANIPULAR VETOR - PUSH: INSERE UM NOVO ELEMENTO DNO FINAL DO VETOR: 
+
+frutas.push('Abacaxi'); 
+document.write('<p> Agora a lista é: ${frutas </p>'); 
