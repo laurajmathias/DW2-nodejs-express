443b6cc7eee1c7800098c27c498f6359559677de
Laura Julia Mathias Amaral <laurajulia408@gmail.com>
2026-08-20 19:29:45 -0300

    código corrigido forEach

--- js-aula-02.1-arrays-e-objetos/03 - array de objetos /script.js
+++ js-aula-02.1-arrays-e-objetos/03 - array de objetos /script.js
@@ -6,42 +6,43 @@ const produto = {
     descricao: "PC moderno e com bom desempenho",
 }; 
 
-document.write('<p>${produto}</p>'); 
-
-//ACESSANDO OS ATRIBUTOS DO OBJETO:
-document.write('<p> $%{produto.nome}, ${produto.marca}, custa R$ ${produto.preco}, <br> ${^produto.descricao} </p>'); 
+// ACESSANDO OS ATRIBUTOS DO OBJETO:
+// Corrigido: removido o % e o ^
+document.write(`<p> ${produto.nome}, ${produto.marca}, custa R$ ${produto.preco}, <br> ${produto.descricao} </p>`); 
 
 // ARRAY DE OBJETOS:
+document.write("<h3>Array de Objetos</h3>"); // Corrigido o '>' extra
 
-document.write("<h3>Array de Objetos> </h3>"); 
 const listaProdutos = [
     {
-    nome: "Computador", 
-    marca: "Lenovo", 
-    preco: 3000, 
-    descricao: "PC moderno e com bom desempenho",
+        nome: "Computador", 
+        marca: "Lenovo", 
+        preco: 3000, 
+        descricao: "PC moderno e com bom desempenho",
     }, 
     {
-    nome: "Tablet", 
-    marca: "Samsumg", 
-    preco: 2000, 
-    descricao: "òtima velocidade de processamento", 
+        nome: "Tablet", 
+        marca: "Samsumg", 
+        preco: 2000, 
+        descricao: "Ótima velocidade de processamento", 
     }, 
     {
-    nome: "Iphone 17", 
-    marca: "Apple", 
-    preco: 17000, 
-    descricao: "Ultra resistente! Não quebra nunca!", 
-    }]; 
+        nome: "Iphone 17", 
+        marca: "Apple", 
+        preco: 17000, 
+        descricao: "Ultra resistente! Não quebra nunca!", 
+    }
+]; 
 
-    // EXIBINDO AS INFORMAÇÕES DOS PRODUTOS COM FOREACH: 
+// EXIBINDO AS INFORMAÇÕES DOS PRODUTOS COM FOREACH: 
+document.write('<h4> Produtos em promoção:</h4>');
 
-    document.write('<h4> Produtos em promoção:</h4>')
-
-    listaProdutos.forEach(produto => {
-
-        document.write(
-              '
-              Produto: ${produto.nome} Marca: ${produto.marca}'
-        )
-    })
+listaProdutos.forEach(produto => {
+    // Corrigido: agora exibe os dados do produto ao invés de repetir o título
+    document.write(`
+        <p><b>Nome:</b> ${produto.nome} <br>
+        <b>Marca:</b> ${produto.marca} <br>
+        <b>Preço:</b> R$ ${produto.preco}</p>
+        <hr>
+    `);
+});
