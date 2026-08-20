f878d806b9ebad82da55164c324de4455479bc3a
Laura Julia Mathias Amaral <laurajulia408@gmail.com>
2026-08-20 19:15:58 -0300

    commitando a aula 2.3

---
+++ js-aula-02.1-arrays-e-objetos/aula-02.3-classes/script.js
@@ -0,0 +1,21 @@
+//CLASSES NO JAVASCRIPT 
+
+    class Carro {
+
+        //Nome de classes devem iniciar com a primeira letra maiúscula. 
+
+        //ATRIBUTOS: 
+
+        constructor(marca, modelo, ano) {
+
+            this.marca = marca; 
+            this.modelo = modelo; 
+            this.ano = ano; 
+        }
+
+        //MÉTODOS: 
+
+        buzinar() {
+            return "Beep! Beep!"; 
+        }
+    }
