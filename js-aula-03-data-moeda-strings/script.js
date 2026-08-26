4c4bd73eb1e2a5a8b8a824b2476ace0eb300e3a8
Laura Julia Mathias Amaral <laurajulia408@gmail.com>
2026-08-26 19:53:42 -0300

    aula delulu

--- js-aula-03-data-moeda-strings/script.js
+++ js-aula-03-data-moeda-strings/script.js
@@ -23,6 +23,81 @@ document.write(`<p> Estamos no ano: ${ano}. </p>`);
 //Exibindo a data completa: 
 document.write(`Data de hoje: ${dia}/${mes}/${ano}.`);
 
+// ADICIONANDO DATAS, MESES E ANOS À DATA ATUAL: 
+//Adicionando 4 dias à data atual: 
+dataAtual.setDate(dataAtual.getDate() + 4); 
+
+//Exibindo a nova data: 
+document.write(`<p> Daqui a 4 dias será dia: ${dataAtual.getDate()}</p>`); 
+
+//Adicionando 4 meses: 
+
+dataAtual.setMonth(dataAtual.getDate() + 4); 
+document.write(`<p> Daqui 4 meses será o mês: ${dataAtual.getMonth() + 1}</p>`);
+
+//Adicionando 4 anos: 
+
+dataAtual.setFullYear(dataAtual.getFullYear() + 4); 
+document.write(`<p> Daqui 4 anos será: ${dataAtual.getFullYear}</p>`); 
+
+//########################################################################################################################################
+
+//FORMATAÇÃO DE NÚMEROS E MOEDAS 
+
+// - NÚMEROS 
+document.write("<h3> Manipulando Números e Moedas: </h3>"); 
+const futuroSalario = 40000.3; 
+document.write(`<p>${futuroSalario}</p>`); 
+//Manipulando o número para mostrar mais casas decimais: 
+document.write(`<p>${futuroSalario.toFixed(2)}</p>`);
+//Para ocultar as casas decimais: 
+document.write(`<p>${futuroSalario.toFixed(0)}</p>`); 
+
+//Como conventer o identificador de casas decimais: 
+document.write(`<p>${futuroSalario.toFixed(2).replace(".", ",")}</p>`); 
+
+//FORMATANDO PARA REAL(R$):
+document.write(`<p>
+    ${futuroSalario.toLocaleString("pt-br", {
+        style: "currency", 
+        currency: "BRL"
+    })}
+    </p>`); 
+    
+    
+//FORMATANDO PARA DÓLAR(USD):
+document.write(`<p>
+    ${futuroSalario.toLocaleString("en", {
+        style: "currency", 
+        currency: "USD"
+    })}
+    </p>`); 
+
+    
+//FORMATANDO PARA EURO(EUR):
+document.write(`<p>
+    ${futuroSalario.toLocaleString("en", {
+        style: "currency", 
+        currency: "EUR"
+    })}
+    </p>`); 
+
+    
+    
+    
+
+
+    
+    
+
+
+
+
+
+
+
+
+
 
 
 
