791f3090f80807ae18993e33f82efe15c7029d29
Laura Julia Mathias Amaral <laurajulia408@gmail.com>
2026-08-27 20:10:53 -0300

    commiting o iniciodo projeor

---
+++ Aula03-Lista-Exercicios/script.js
@@ -0,0 +1,9 @@
+class Heroi {
+
+    constructor(nome, vida, velocidade, forca) {
+        this.nome = nome; 
+        this.vida = vida; 
+        this.velocidade = velocidade; 
+        this.forca = forca; 
+    }
+}
