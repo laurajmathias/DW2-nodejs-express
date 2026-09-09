09b35c60a191fb54e8fe4a86f39d93d61c9ba345
laurajmathias <laurajulia408@gmail.com>
2026-09-09 19:14:57 -0300

    teste commit

--- aula-04-introdução-nodejs/index.js
+++ aula-04-introdução-nodejs/index.js
@@ -1,102 +1,167 @@
 // arquivo principal do backend
 
-// Configurando o EJS: 
-app.set('view engine', 'ejs') // rendereiza as páginas do site 
+// IMPORTANDO O EXPRESS PARA O PROJETO: 
 
-//AQUI IRÃO AS ROTAS DO SITE 
-//ROTA PRINCIPAL 
+const express = require("express") // - Forma clássica (CommomJS Modules); 
+
+const app = express(); // Criando uma instancia do express, pega o express e carrega na variável app. 
+
+// Configurando o EJS: 
+
+app.set('view engine', 'ejs') // renderiza as páginas do site 
+
+
+
+// AQUI IRÃO AS ROTAS DO SITE 
+
+// ROTA PRINCIPAL 
+
 // .get() -> Cria uma rota da aplicação 
 
 // Exercícios de rota - 
+
 // 1. Criar as seguintes rotas no site: 
+
 // /serviços, /clientes, /perfil 
 
 
+
 app.get("/", (req, res) => {
+
     res.render('index')
+
 }); 
 
-//|rota de produtos: 
-app.get("Produtos", (req, res) => {
-    //Lista de produtos (dados mockados)
-    //Futuramente os dados virão do banco de dados
-    //Array de objetos
+
+
+// Rota de produtos: 
+
+app.get("/Produtos", (req, res) => {
+
+    // Lista de produtos (dados mockados)
+
+    // Futuramente os dados virão do banco de dados
+
+    // Array de objetos
 
     const produtos = [
+
         {nome: "Computador", marca: "Lenovo", preco: 3500}, 
+
         {nome: "Celular", marca: "Samsung", preco: 4100}, 
+
         {nome: "Notebook", marca: "Acer", preco: 5100}, 
+
         {nome: "Tablet", marca: "Asus", preco: 2400},  
 
     ];
-        res.render("produtos", {
-             //Enviando a lista de produtos para a página: 
-             produtos: produtos, 
 
-        }); 
-}); //localhost8080/produtos
+    res.render("produtos", {
 
-app.get("Servicos", (req, res) => {
-    res.render('/servicos')
-} ); 
+        // Enviando a lista de produtos para a página: 
 
-app.get("Clientes", (req, res) => {
+        produtos: produtos, 
+
+    }); 
+
+}); // localhost:8080/Produtos
+
+
+
+app.get("/Servicos", (req, res) => {
+
+    res.render('servicos')
+
+}); 
+
+
+
+app.get("/Clientes", (req, res) => {
+
     const clientes =
+
     [
-        {nome: "Diego Max", cpf: "999.999.999-99",}, 
+
+        {nome: "Diego Max", cpf: "999.999.999-99"}, 
+
         {nome: "Gregory Sopuza", cpf: "999.999.999-99"}, 
+
         {nome: "Laura Júlia", cpf: "999.999.999-99"}, 
+
         {nome: "Mario Armando", cpf: "999.999.999-9"}, 
-        
+
     ]; 
-    res.render('/clientes')
+
+    res.render('clientes', {
+
+        clientes: clientes
+
+    })
+
 }); 
 
-app.get("Perfil", (re1, res) => {
-    res.render('/perfil')
+
+
+app.get("/Perfil", (req, res) => {
+
+    res.render('perfil')
+
 }); 
+
+
 
 // 2 - Criar páginas EJS para cada uma. 
-// 3 - Incluir todfas as rotas no menu principal 
-// 4 - incluir botão de voltar em todas as páginas
 
-// IMPORTANDO O EXPRESS PARA O PROJETO: 
-const express = require("express") // - Forma clássica (CommomJS Modules); 
-const app = express(); // Criando uma instancia do express, pega op express e carrega na variável app. 
+// 3 - Incluir todas as rotas no menu principal 
+
+// 4 - Incluir botão de voltar em todas as páginas
+
+
 
 // Método do Express para iniciar o servidor back-end: 
-//app.listen()
+
+// app.listen()
+
+
 
 // Iniciando o servidor na porta 8080: 
+
 const port = 8080; 
+
 app.listen(port, (error) => {
+
     // Tratando erros de inicialização: 
+
     if (error) {
+
         // Verifica se erro tem algum valor = true. 
-        console.log(`Ocorreu um erro ao iniciar o servidor.  Erro:  ${error}`); 
+
+        console.log(`Ocorreu um erro ao iniciar o servidor. Erro: ${error}`); 
+
     } else {
+
         console.log(`Servidor iniciado com sucesso em: http://localhost:${port}`); 
+
     }
 
-
 });
- 
 
-//nodemon - biblioteca para inciar o servidor automático: 
+
+
+// nodemon - biblioteca para iniciar o servidor automático: 
+
 // terminal = npm install nodemon; 
 
-//para configurar essa biblioteca -> abre o package.json -> vai estar em dependencies -> cria o script pro projeto 
+// para configurar essa biblioteca -> abre o package.json -> vai estar em dependencies -> cria o script pro projeto 
 
 // ao final do script test, põe uma virgula, desce uma linha e escreve: 
+
 // "npx nodemon index.js" - entre as aspas mesmo 
 
 
 
-// joga npm start no terminal e ele inicializa o servidor a cada modifição do código 
+// joga npm start no terminal e ele inicializa o servidor a cada modificação do código 
+
 // o npx é usado para rodar/executar as bibliotecas; 
 
-//Instalar biblioteca EJS para inicializar as páginas do site... npm install ejs 
-
-
-
-
+// Instalar biblioteca EJS para inicializar as páginas do site... npm install ejs
