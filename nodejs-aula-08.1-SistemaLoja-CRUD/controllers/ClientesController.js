2c283961a4020b05af72e29fc1991af9f47b7f80
laurajmathias <laurajulia408@gmail.com>
2026-09-24 19:29:53 -0300

    commit da continuação da aula 8

--- nodejs-aula-08.1-SistemaLoja-CRUD/controllers/ClientesController.js
+++ nodejs-aula-08.1-SistemaLoja-CRUD/controllers/ClientesController.js
@@ -1,21 +1,47 @@
+
 // ROTA CLIENTES
+
 
 import express from "express";
 import Cliente from "../models/Cliente.js";
 
+
 const rota = express.Router();
 
-// Rota de clientes
+
+// Rota de listagem de clientes
 rota.get("/clientes", function (req, res) {
-
     Cliente.findAll().then(clientes => {
-
         res.render("clientes", {
             clientes: clientes,
         });
-
+    }).catch(error => {
+        console.log("Ocorreu um erro ao listar os clientes: " + error);
     });
-
 });
 
+
+// ROTA DE CADASTRO DE CLIENTES (Alterado para POST)
+rota.post("/clientes/cadastrar", (req, res) => {
+    // Capturando os dados vindos do formulário e gravando nas variáveis
+    const nome = req.body.nome;
+    const cpf = req.body.cpf;
+    const endereco = req.body.endereco;
+
+
+    // Chamando o model para gravar os dados no banco:
+    Cliente.create({
+        nome: nome,
+        cpf: cpf,
+        endereco: endereco,
+    }).then(() => {
+        res.redirect("/clientes");
+    }).catch(error => {
+        console.log(`Ocorreu um erro ao cadastrar o cliente. Erro: ${error}`);
+    });
+});
+
+
 export default rota;
+
+
--- nodejs-aula-08.1-SistemaLoja-CRUD/index.js
+++ nodejs-aula-08.1-SistemaLoja-CRUD/index.js
@@ -1,3 +1,4 @@
+
 // Importando o Express com ES6 Modules
 import express from "express";
 // Importando o arquivo de conexão do Sequelize
@@ -9,19 +10,26 @@ import PedidosController from "./controllers/PedidosController.js";
 import ClientesController from "./controllers/ClientesController.js";
 import ProdutoController from "./controllers/ProdutosController.js";
 
+
 // importando models
 import Cliente from "./models/Cliente.js";
 import Pedido from "./models/Pedido.js";
-import Produto from "./models/Produto.js"; 
+import Produto from "./models/Produto.js";
 // Configurações do Express
+//Configurando o express para permitir dados através de formulários:
+app.use(express.urlencoded({ extended: false}));
+ 
+
+
 // Define o EJS como Renderizador de páginas
-app.set("view engine", "ejs");1
+app.set("view engine", "ejs");
 // Define o uso da pasta "public" para uso de arquivos estáticos
 app.use(express.static("public"));
 // Definindo o uso das rotas dos Controllers
 app.use("/", PedidosController);
-app.use("/", ClientesController); 
-app.use("/", ProdutoController); 
+app.use("/", ClientesController);
+app.use("/", ProdutoController);
+
 
 // REALIZANDO A CONEXÃO COM O BANCO DE DADOS
 connection.authenticate().then(() => {
@@ -32,17 +40,20 @@ connection.authenticate().then(() => {
     console.log(`Ocorreu um erro ao se conectar ao banco de dados. Erro: ${error}`)
 });
 
-//CRIANDO BANCO DER DADOS SE ELE NÃO EXISTIR 
-const DB_NAME = "loja"; 
+
+//CRIANDO BANCO DER DADOS SE ELE NÃO EXISTIR
+const DB_NAME = "loja";
 connection.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME};`).then(() => {
   console.log(`O banco de dados ${DB_NAME} está criado!`)
 }).catch((error) => {
-  console.log(`Ocorreu um erro ao criar o banco de dados. Erro: ${error}`); 
+  console.log(`Ocorreu um erro ao criar o banco de dados. Erro: ${error}`);
 })
 // ROTA PRINCIPAL
 app.get("/", function (req, res) {
   res.render("index");
 });
+
+
 
 
 // INICIA O SERVIDOR NA PORTA 8080
@@ -53,3 +64,5 @@ app.listen(8080, function (erro) {
     console.log("Servidor iniciado com sucesso!");
   }
 });
+
+
